e59e41ea7c319b388adba423144e5a3716c9d3ea
Owent Ovandy <owent.ovandy7777@gmail.com>
2024-11-11 15:22:46 +0700

    added upload services and example controllers

---
+++ services/upload.py
@@ -0,0 +1,27 @@
+import boto3
+import os
+
+R2_ACCESS_KEY_ID = os.getenv("R2_ACCESS_KEY_ID")
+R2_SECRET_ACCESS_KEY = os.getenv("R2_SECRET_ACCESS_KEY")
+R2_BUCKET_NAME = os.getenv("R2_BUCKET_NAME")
+R2_ENDPOINT_URL = os.getenv("R2_ENDPOINT_URL")
+R2_DOMAINS = os.getenv("R2_DOMAINS")
+
+
+class UploadService:
+    def __init__(self):
+        self.s3_client = boto3.client(
+            "s3",
+            endpoint_url=R2_ENDPOINT_URL,
+            aws_access_key_id=R2_ACCESS_KEY_ID,
+            aws_secret_access_key=R2_SECRET_ACCESS_KEY,
+        )
+
+    def upload_file(self, file, filename):
+        try:
+            self.s3_client.upload_fileobj(file, R2_BUCKET_NAME, filename)
+            file_url = f"{R2_DOMAINS}/{filename}"
+            return file_url
+        except Exception as e:
+            print(f"Failed to upload file : {e}")
+            return False
