78d1172b20868b864eaa7026de7fd9438399ba4e
Owent Ovandy <owent.ovandy7777@gmail.com>
2024-11-19 00:33:50 +0700

    feat: added more validation

--- schemas/overall_review_schema.py
+++ schemas/overall_review_schema.py
@@ -1,15 +1,45 @@
 create_review_schema = {
-    "score": {
-        "type": "int",
-        "required": True,
+    "restaurant_id": {"type": "integer", "required": True},
+    "score": {"type": "integer", "min": 0, "max": 5, "nullable": True},
+    "review_body": {"type": "string", "nullable": True},
+    "review_status": {"type": "string", "nullable": True},
+}
+
+submit_review_schema = {
+    "review": {
+        "type": "dict",
+        "schema": {
+            "score": {"type": "integer", "min": 0, "max": 5},
+            "review_body": {"type": "string"},
+            "review_status": {"type": "string"},
+        },
     },
-    "review_status": {
-        "type": "string",
-        "maxlength": 255,
-        "required": True,
+    "food": {
+        "type": "dict",
+        "schema": {
+            "food_score": {"type": "integer", "min": 0, "max": 5},
+            "food_comment": {"type": "string", "nullable": True},
+        },
     },
-    "review_body": {
-        "type": "string",
-        "required": False,
-    }
+    "service": {
+        "type": "dict",
+        "schema": {
+            "service_score": {"type": "integer", "min": 0, "max": 5},
+            "service_comment": {"type": "string", "nullable": True},
+        },
+    },
+    "ambience": {
+        "type": "dict",
+        "schema": {
+            "ambience_score": {"type": "integer", "min": 0, "max": 5},
+            "ambience_comment": {"type": "string", "nullable": True},
+        },
+    },
+    "media": {
+        "type": "list",
+        "schema": {
+            "type": "dict",
+            "schema": {"location": {"type": "string", "nullable": True}},
+        },
+    },
 }
