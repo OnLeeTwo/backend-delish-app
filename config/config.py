8caf0d64b5c1251a6a5bc35da8e39872eafed843
Owent Ovandy <owent.ovandy7777@gmail.com>
2024-11-20 18:35:50 +0700

    prepare to deploy

--- config/config.py
+++ config/config.py
@@ -7,4 +7,3 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_DATABASE')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    print(SQLALCHEMY_DATABASE_URI)
